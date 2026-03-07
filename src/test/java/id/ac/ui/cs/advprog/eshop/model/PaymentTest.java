117053c372dc7b6c3df893aef5c06bb380f0d4d6
susshikan <muhamadh123g@gmail.com>
2026-03-08 05:24:48 +0700

    [RED] Add Payment model skeleton

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -8,42 +8,53 @@ import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PaymentTest {
 
-    Payment payment;
-    Map<String, String> paymentData;
+    private Map<String, String> paymentData;
 
     @BeforeEach
     void setUp() {
-        payment = new Payment();
         paymentData = new HashMap<>();
         paymentData.put("voucherCode", "ESHOP1234ABC5678");
-
-        this.payment.setId("pay-1");
-        this.payment.setMethod("VOUCHER_CODE");
-        this.payment.setStatus("SUCCESS");
-        this.payment.setPaymentData(paymentData);
     }
 
     @Test
-    void getId() {
-        assertEquals("pay-1", this.payment.getId());
+    void testCreatePayment() {
+        Payment payment = new Payment("pay-1", "VOUCHER_CODE", "SUCCESS", this.paymentData);
+
+        assertEquals("pay-1", payment.getId());
+        assertEquals("VOUCHER_CODE", payment.getMethod());
+        assertEquals("SUCCESS", payment.getStatus());
+        assertSame(this.paymentData, payment.getPaymentData());
     }
 
     @Test
-    void getMethod() {
-        assertEquals("VOUCHER_CODE", this.payment.getMethod());
+    void testCreatePaymentRejectedStatus() {
+        Payment payment = new Payment("pay-1", "VOUCHER_CODE", "REJECTED", this.paymentData);
+
+        assertEquals("REJECTED", payment.getStatus());
     }
 
     @Test
-    void getStatus() {
-        assertEquals("SUCCESS", this.payment.getStatus());
+    void testCreatePaymentInvalidStatus() {
+        assertThrows(IllegalArgumentException.class, () ->
+                new Payment("pay-1", "VOUCHER_CODE", "MEOW", this.paymentData));
     }
 
     @Test
-    void getPaymentData() {
-        assertSame(paymentData, this.payment.getPaymentData());
-        assertEquals("ESHOP1234ABC5678", this.payment.getPaymentData().get("voucherCode"));
+    void testSetStatusToSuccess() {
+        Payment payment = new Payment("pay-1", "VOUCHER_CODE", "REJECTED", this.paymentData);
+        payment.setStatus("SUCCESS");
+
+        assertEquals("SUCCESS", payment.getStatus());
+    }
+
+    @Test
+    void testSetStatusInvalidStatus() {
+        Payment payment = new Payment("pay-1", "VOUCHER_CODE", "SUCCESS", this.paymentData);
+
+        assertThrows(IllegalArgumentException.class, () -> payment.setStatus("MEOW"));
     }
 }
